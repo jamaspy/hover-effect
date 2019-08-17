519bb39d36eff24b2b7199328b26b68899ef6583
James Aspinall <aspin@me.com>
2019-08-17 15:40:27 +1000

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+new hoverEffect({
+    parent: document.querySelector(".distortion"),
+    intensity: 0.2,
+    image1: './image1.jpg',
+    image2: './image2.jpg',
+    displacementImage: './diss.png'
+})
